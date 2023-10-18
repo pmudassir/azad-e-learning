32a8574d689ea2cf9273597889fa5063566fb247
mFaYizp <fayizfaz78@gmail.com>
2023-10-18 10:50:07 +0530

    cards aligning changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,18 +1,16 @@
-
-
-import React from 'react'
-import Hero from '../../components/heroSection/Hero'
-import Navbar from '../../components/navbar/Navbar'
-import Card from "../../components/widget/Card";
+import React from "react";
+import Hero from "../../components/heroSection/Hero";
+import Navbar from "../../components/navbar/Navbar";
+import Card from "../../components/card/Card";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
-    <Card />
+      <Card />
     </>
-  )
-}
+  );
+};
 
 export default Home;
