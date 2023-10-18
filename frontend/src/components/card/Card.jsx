8ad67cbb680779e924d83a1807dc9dc35e0a2a89
mFaYizp <fayizfaz78@gmail.com>
2023-10-18 22:50:56 +0530

    workinng on courses

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import "./card.css";
+import { Link } from "react-router-dom";
 
 const cardData = [
   {
     id: 1,
     title: "Foundation Programs",
+    link: "/courses",
     imageUrl:
       "https://img.icons8.com/external-smashingstocks-hand-drawn-color-smashing-stocks/100/external-Abacus-education-smashingstocks-hand-drawn-color-smashing-stocks.png",
   },
   {
     id: 2,
     title: "Pre-Primary TTC",
+    link: "/",
     imageUrl: "https://i.ibb.co/MpCD2wf/teacher-5190740.png",
   },
   {
     id: 3,
     title: "Montessori <br/> TTC",
+    link: "/",
     imageUrl:
       "https://img.icons8.com/external-justicon-lineal-color-justicon/128/external-business-presentation-back-to-school-justicon-lineal-color-justicon.png",
   },
   {
     id: 4,
     title: "Hindi <br/> TTC",
+    link: "/",
     imageUrl: "https://i.ibb.co/PhdRvqr/hindi.png",
   },
   {
     id: 4,
     title: "Competitive Goals",
+    link: "/",
     imageUrl:
       "https://img.icons8.com/external-goofy-flat-kerismaker/192/external-Goals-business-goofy-flat-kerismaker.png",
   },
@@ -42,19 +48,21 @@ const Card = () => {
       <div className="cards">
         {cardData.map((item) => (
           <div className="cardItem" key={item.id}>
-            <div className="cardItemContent">
-              <img
-                width="95"
-                height="95"
-                className="card-icon"
-                src={item.imageUrl}
-                alt="course-avatar"
-              />
-              <h6
-                className="card-title"
-                dangerouslySetInnerHTML={{ __html: item.title }}
-              ></h6>
-            </div>
+            <Link to={item.link} style={{textDecoration:"none",color:"black"}}>
+              <div className="cardItemContent">
+                <img
+                  width="95"
+                  height="95"
+                  className="card-icon"
+                  src={item.imageUrl}
+                  alt="course-avatar"
+                />
+                <h6
+                  className="card-title"
+                  dangerouslySetInnerHTML={{ __html: item.title }}
+                ></h6>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
