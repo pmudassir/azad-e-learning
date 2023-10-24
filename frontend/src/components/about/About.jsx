43ef0d10b11c2f8e743b6497e84384df3e4a8dd3
pmudassir <pmuddasir@gmail.com>
2023-10-24 22:50:26 +0530

    about section completed

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -3,15 +3,31 @@ import './about.css';
 
 const About = () => {
     return (
-        <div className="about-us">
-            <div className="about-us-content">
-                <h2>About Us</h2>
-                <p>
+        <div class="responsive-container-block bigContainer">
+            <div class="responsive-container-block Container">
+                <p class="text-blk heading">
+                    About Us
+                </p>
+                <p class="text-blk subHeading">
                     We are Kerala's premier E-Learning Platform, dedicated to providing high-quality education
                     anytime, anywhere. Our mission is to empower learners through innovative and accessible
                     online courses. With a team of expert educators and cutting-edge technology, we strive for
                     excellence and a bright future for all our students.
                 </p>
+                {/* <div class="social-icons-container">
+                    <a class="social-icon">
+                        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb33.png" />
+                    </a>
+                    <a class="social-icon">
+                        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb34.png" />
+                    </a>
+                    <a class="social-icon">
+                        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png" />
+                    </a>
+                    <a class="social-icon">
+                        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb36.png" />
+                    </a>
+                </div> */}
             </div>
         </div>
     );
