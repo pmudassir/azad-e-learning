7c67d4c2e072acc537648733a118c7944d7fd6e5
pmudassir <pmuddasir@gmail.com>
2023-11-20 15:48:06 +0530

    auth err msgs done

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { resetUser } from "../../store/userSlice";
