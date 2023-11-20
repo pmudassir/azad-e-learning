9a172030f69235d4cb1226b8d02a56d139790e4f
pmudassir <pmuddasir@gmail.com>
2023-11-20 12:45:38 +0530

    g auth commented

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const User = require("./models/User");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const User = require("./models/User");
 require("dotenv").config();
 const app = express();
 const authRoute = require("./routes/auth");
@@ -27,8 +27,6 @@ app.use(
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(passport.initialize());
-app.use(passport.session());
 
 passport.use(
   new GoogleStrategy(
@@ -62,37 +60,67 @@ passport.use(
   )
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: "http://localhost:4000/auth/google/callback",
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         const user = await User.findOne({ email: profile.emails[0].value });
+//         if (!user) {
+//           const newUser = new User({
+//             username: profile.displayName,
+//             email: profile.emails[0].value,
+//             image: profile.photos[0].value,
+//           });
+//           const savedUser = await newUser.save();
+//           const accessToken = jw.sign({ userId: savedUser._id }, process.env.JWT_SECRET);
+//           return done(null, profile, accessToken);
+//         }
+//         const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+//         return done(null, profile, accessToken);
+//       } catch (error) {
+//         console.log(error);
+//         return done(error);
+//       }
+//     }
+//   )
+// );
 
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
+
+// passport.deserializeUser((user, done) => {
+//   done(null, user);
+// });
 
 // Routes
 app.use("/api/auth", authRoute);
 app.use("/api/forms", formsRoute);
 app.use("/api/course", courseRoute);
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+// app.get(
+//   "/auth/google",
+//   passport.authenticate("google", { scope: ["profile", "email"] })
+// );
 
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/auth" }),
-  (req, res) => {
-    res.redirect("http://localhost:3000/");
-  }
-);
+// app.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", { failureRedirect: "/auth" }),
+//   (req, res) => {
+//     res.redirect("http://localhost:3000/");
+//   }
+// );
 
-app.get("/logout", (req, res) => {
-  req.logout(() => {
-    res.redirect("/");
-  });
-});
+// app.get("/logout", (req, res) => {
+//   req.logout(() => {
+//     res.redirect("/");
+//   });
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
